d8b815e93709598efd0331b974dc23a36faabc4f
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-05 08:53:24 -0700

    Implement getAlbums

--- server/plexAPI/controllers/getAlbums.js
+++ server/plexAPI/controllers/getAlbums.js
@@ -1,5 +1,19 @@
+const plexConfig = require('../plexConfig');
+const axios = require('axios');
+
 const getAlbums = (req, res) => {
-  res.end();
+  const url = `${plexConfig.baseURL}:${plexConfig.port}/library/sections/9/all`;
+  const params = {
+    'X-Plex-Token': plexConfig.plexToken
+  };
+
+  axios.get(url, { params })
+    .then ((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 module.exports = getAlbums;
