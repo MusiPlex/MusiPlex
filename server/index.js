20f18f9ac79a98e6f127df09c08593f014af1bec
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-05 08:34:55 -0700

    Point Server to Dist Folder

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3004;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
 
 app.listen(port, () => {
   console.log(`MusiPlex listening at http://localhost:${port}`);
