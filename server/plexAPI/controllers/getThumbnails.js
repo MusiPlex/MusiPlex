be151dd40448e71c263e3b6a32de790f478e8ce6
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-06 18:09:29 -0700

    Add Caching of Resources

--- server/plexAPI/controllers/getThumbnails.js
+++ server/plexAPI/controllers/getThumbnails.js
@@ -4,30 +4,34 @@ const path = require('path');
 const plexConfig = require('../plexConfig');
 
 const getThumbnails = async (req, res) => {
-  const url = `${plexConfig.baseURL}:${plexConfig.port}/photo/:/transcode`;
-  const params = {
-    'width': '240',
-    'height': '240',
-    'minSize': '1',
-    'upscale': '1',
-    'url': req.query.url,
-    'X-Plex-Token': plexConfig.plexToken,
-  };
-  try {
-    const response = await axios({
-      method: 'GET',
-      url: url,
-      params: params,
-      responseType: 'stream',
-    });
-    let outName =` ${req.query.url.split('/').slice(-1)}.jpg`
-    let outPath = path.join(__dirname, '..', '..', '..', 'assets', outName)
-    const w = response.data.pipe(fs.createWriteStream(outPath));
-    w.on('finish', () => {
-      res.sendFile(outPath);
-    });
-  } catch (err) {
-    throw new Error(err);
+  let outName =` ${req.query.url.split('/').slice(-1)}.jpg`
+  let outPath = path.join(__dirname, '..', '..', '..', 'assets', outName)
+  if (fs.existsSync(outPath)) {
+    res.sendFile(outPath);
+  } else {
+    const url = `${plexConfig.baseURL}:${plexConfig.port}/photo/:/transcode`;
+    const params = {
+      'width': '240',
+      'height': '240',
+      'minSize': '1',
+      'upscale': '1',
+      'url': req.query.url,
+      'X-Plex-Token': plexConfig.plexToken,
+    };
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: url,
+        params: params,
+        responseType: 'stream',
+      });
+      const w = response.data.pipe(fs.createWriteStream(outPath));
+      w.on('finish', () => {
+        res.sendFile(outPath);
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 };
 
