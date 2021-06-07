57b0b1abd64ce1ec0e10b5c17fa45ad902b64d90
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-06 18:43:37 -0700

    Added Styling for Album View

--- client/src/styles/GeneralStyles.js
+++ client/src/styles/GeneralStyles.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 
 const SixColumnLayout = styled.div`
-  -webkit-columns: 6 200px;
-     -moz-columns: 6 200px;
-          columns: 6 200px;
+  padding-left: 5%;
+  padding-right: 5%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
+const AlbumView = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 2%;
+  width: 200px;
+`;
+
+const AlbumTitle = styled.h1`
+  font-size: 0.8em;
+  text-align: center;
+  text-overflow: ellipsis;
+`;
+
+
 const generalStyles = {
-  SixColumnLayout
+  SixColumnLayout,
+  AlbumView,
+  AlbumTitle
 };
 
 export default generalStyles;
