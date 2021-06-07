99915e16a662f9640e7617b6e596b92f44fff677
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-06 18:44:00 -0700

    Create Global Styling

---
+++ client/src/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const body = styled.div`
+  font-family: 'Roboto', sans-serif;
+`;
+
+
+const globalStyles = {
+  body
+};
+
+export default globalStyles;
